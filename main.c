9c3b9c9dbf31816fb5d9f4c23984e54c288dcb01
kwnnr <kwinnerzed@gmail.com>
2024-05-21 18:11:37 +0300

    Arrange the columns of the matrix by non-decreasing the minimum elements of the columns

--- main.c
+++ main.c
@@ -2,18 +2,18 @@
 #include <windows.h>
 #include "C:\Users\kwinn\CLionProjects\lab_15\libs\data_structures\matrix\matrix.h"
 
-int getMax(int *a, int n) {
-    int max = a[0];
+int getMin(int *a, int n) {
+    int min = a[0];
     for (int i = 1; i < n; i++) {
-        if (a[i] > max) {
-            max = a[i];
+        if (a[i] < min) {
+            min = a[i];
         }
     }
-    return max;
+    return min;
 }
 
 void sortRowsByMinElement(matrix m) {
-    insertionSortRowsMatrixByRowCriteria(m, getMax);
+    insertionSortColsMatrixByColCriteria(m, getMin);
 }
 
 int main() {
